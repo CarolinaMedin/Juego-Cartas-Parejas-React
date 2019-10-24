9f95d5de8f7f022027925bae76f068e7b304c0fb
carito <carolina.medina>
2019-10-23 22:57:37 -0300

    cambios componente cartaja y css

--- src/Carta.js
+++ src/Carta.js
@@ -7,12 +7,12 @@ export default class Carta extends Component{
     render(){
         return (
             <div className = "carta">
-            <div id="flip-container">
+            {/* <div id="flip-container">
             <div id="card1" className="card">
             <div className="front"></div>
             <div className="back"></div>
             </div>
-            </div>
+            </div> */}
                
             <i className={`fa ${this.props.icono} fa-5x`}></i>
             </div>
