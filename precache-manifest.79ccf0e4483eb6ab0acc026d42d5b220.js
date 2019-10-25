21d65b78504a80d4bcd1d57b86cffe0de682f71e
carito <carolina.medina>
2019-10-24 23:23:22 -0300

    Updates

---
+++ precache-manifest.79ccf0e4483eb6ab0acc026d42d5b220.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aadd7c8bbeb620712f3078037ceb56b",
+    "url": "/CarolinaMedin/Juego-cartas-parejas-react.git/index.html"
+  },
+  {
+    "revision": "7b16a4268fa329547ed3",
+    "url": "/CarolinaMedin/Juego-cartas-parejas-react.git/static/css/2.3392d209.chunk.css"
+  },
+  {
+    "revision": "cd0f7dab23477a760130",
+    "url": "/CarolinaMedin/Juego-cartas-parejas-react.git/static/css/main.4a8ba5da.chunk.css"
+  },
+  {
+    "revision": "7b16a4268fa329547ed3",
+    "url": "/CarolinaMedin/Juego-cartas-parejas-react.git/static/js/2.95a616b1.chunk.js"
+  },
+  {
+    "revision": "cd0f7dab23477a760130",
+    "url": "/CarolinaMedin/Juego-cartas-parejas-react.git/static/js/main.82f935b1.chunk.js"
+  },
+  {
+    "revision": "ab85b7ea8bdd1bda7fe4",
+    "url": "/CarolinaMedin/Juego-cartas-parejas-react.git/static/js/runtime-main.ab34d0be.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/CarolinaMedin/Juego-cartas-parejas-react.git/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/CarolinaMedin/Juego-cartas-parejas-react.git/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/CarolinaMedin/Juego-cartas-parejas-react.git/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/CarolinaMedin/Juego-cartas-parejas-react.git/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/CarolinaMedin/Juego-cartas-parejas-react.git/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
